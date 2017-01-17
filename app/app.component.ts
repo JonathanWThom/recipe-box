ad3f770dba121714ac5f822c6a09b5daaf6b3765
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-01-17 09:35:53 -0800

    Add recipe model

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,10 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Recipe Box</h1>
+  <div class="container">
+    <h1>Recipe Box</h1>
+    <ul>
+      <li>{{firstRecipe.title}}</li>
+    </ul>
+  </div>
   `
 })
 
 export class AppComponent {
+  firstRecipe: Recipe = new Recipe("Fluffy French Toast", "Bread, eggs, milk, flour, cinnamon, oil", "Fry it!");
+}
 
+export class Recipe {
+  constructor(public title: string, public ingredients: string, public directions: string) { }
 }
